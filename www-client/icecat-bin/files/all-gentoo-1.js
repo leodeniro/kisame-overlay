cf4794e527525ab02360ca1c2426030bf90c053a
kisame <kisame@protonmail.ch>
2017-10-16 01:42:21 -0400

    added icecat stuff

---
+++ www-client/icecat-bin/files/all-gentoo-1.js
@@ -0,0 +1,6 @@
+lockPref("app.update.auto", false);
+lockPref("app.update.enabled", false);
+lockPref("intl.locale.matchOS",                true);
+defaultPref("browser.display.use_system_colors",  true);
+defaultPref("browser.shell.checkDefaultBrowser",  false);
+pref("browser.EULA.override",              true);
